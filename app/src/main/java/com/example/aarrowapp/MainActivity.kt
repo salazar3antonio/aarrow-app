a0823400068aef82c6345719d7cc3eacd1b008b3
Antonio Salazar III <salazar3antonio@gmail.com>
2019-07-30 21:08:54 -0500

    fun: Added Recycler View for Employee Fragment

--- app/src/main/java/com/example/aarrowapp/MainActivity.kt
+++ app/src/main/java/com/example/aarrowapp/MainActivity.kt
@@ -3,15 +3,12 @@ package com.example.aarrowapp
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
-import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.example.aarrowapp.ui.main.SectionsPagerAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,15 +17,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-
-        val viewPager: androidx.viewpager.widget.ViewPager = findViewById(R.id.view_pager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
 
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
-
         fab.setOnClickListener {
             val intent = Intent(this@MainActivity, NewEmployeeActivity::class.java)
             startActivityForResult(intent, newEmployeeRequestCode)
@@ -46,7 +41,6 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
 
     companion object {
         const val newEmployeeRequestCode = 1
--- app/src/main/java/com/example/aarrowapp/ui/main/MainEmployeeFragment.kt
+++ app/src/main/java/com/example/aarrowapp/ui/main/MainEmployeeFragment.kt
@@ -5,16 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aarrowapp.EmployeeListAdapter
 import com.example.aarrowapp.R
+import com.example.aarrowapp.database.EmployeeViewModel
 
 public class MainEmployeeFragment : androidx.fragment.app.Fragment() {
 
     //newInstance w/ bundle for this Fragment
     //RecyclerView of EmployeeEntity list items
     //Adapter of Employees List. Should be from Room DB
+    private lateinit var employeeViewModel: EmployeeViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main_employees, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_main_employees, container, false)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.rv_main_employees)
+        val adapter = EmployeeListAdapter(requireContext())
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        employeeViewModel = ViewModelProviders.of(this).get(EmployeeViewModel::class.java)
+        employeeViewModel.allEmployees.observe(this, Observer { employees ->
+            employees?.let { adapter.setEmployees(it) }
+        })
+
+        return view
     }
 
     companion object {
